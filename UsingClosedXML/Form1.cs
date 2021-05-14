eac1819cb8884be0e93aded10703ef9165453d5c
ohin6 <owen.williams-4@postgrad.manchester.ac.uk>
2021-05-14 16:05:37 +0100

    Import file to excel

--- UsingClosedXML/Form1.cs
+++ UsingClosedXML/Form1.cs
@@ -51,46 +51,60 @@ namespace UsingClosedXML
       
         private void button2_Click(object sender, EventArgs e)
         {
-            IXLWorkbook wb = new XLWorkbook();//Create workbook used closedXML
-            IXLWorksheet ws = wb.Worksheets.Add("Sample Sheet");//In workbook create worksheet and give name
-                                                                //To delete worksheet ---> wb.Worksheet("Sample Sheet").Delete();
 
-            //Give headers using textbox inputs
-
-            ws.Cell(1, 1).Value = box_0101.Text;
-            ws.Cell(1, 2).Value = box_0102.Text;
-            ws.Cell(1, 3).Value = box_0103.Text;
-            ws.Cell(1, 4).Value = box_0104.Text;
-            ws.Cell(1, 5).Value = box_0105.Text;
-            ws.Cell(1, 6).Value = box_0106.Text;
-            ws.Cell(1, 7).Value = box_0107.Text;
-            ws.Cell(1, 8).Value = box_0108.Text;
+            string[] filePath = Lbl_1.Text.Split('\n');
+            foreach (string sFileName in filePath)
+            {
+                //Create workbook used closedXML
+                IXLWorkbook wb = new XLWorkbook(@"Y:\Liverpool projects\Windows form app\Ruth\Auto_OH166_Chip_1_Oncomine_Myeloid_hods-department_500.bcmatrix.xlsx");//Import file change to "sFileName"
+                IXLWorksheet ws = wb.Worksheets.Add();//In workbook create worksheet and give name
+                                                      //To delete worksheet ---> wb.Worksheet("Sample Sheet").Delete();
 
 
 
-            // Add a bunch of numbers to filter
-            ws.Cell("a2").SetValue(10)
-                         .CellBelow().SetValue(2)
-                         .CellBelow().SetValue(3)
-                         .CellBelow().SetValue(3)
-                         .CellBelow().SetValue(5)
-                         .CellBelow().SetValue(1)
-                         .CellBelow().SetValue(4);
-            ws.Cell("b2").SetValue("a1")
-                         .CellBelow().SetValue("a")
-                         .CellBelow().SetValue("b")
-                         .CellBelow().SetValue("c")
-                         .CellBelow().SetValue("d")
-                         .CellBelow().SetValue("e")
-                         .CellBelow().SetValue("f");
+               
 
-            // Add filters
-            ws.RangeUsed().SetAutoFilter().Column(1).LessThan(4);
-            ws.AutoFilter.Sort(1);
+
+                //Give headers using textbox inputs
+
+                ws.Cell(1, 1).Value = box_0101.Text;
+                ws.Cell(1, 2).Value = box_0102.Text;
+                ws.Cell(1, 3).Value = box_0103.Text;
+                ws.Cell(1, 4).Value = box_0104.Text;
+                ws.Cell(1, 5).Value = box_0105.Text;
+                ws.Cell(1, 6).Value = box_0106.Text;
+                ws.Cell(1, 7).Value = box_0107.Text;
+                ws.Cell(1, 8).Value = box_0108.Text;
 
 
 
-            wb.SaveAs(@"Y:\Liverpool projects\Windows form app\Ruth\test.xlsx");
+                // Add a bunch of numbers to filter
+                ws.Cell("a2").SetValue(10)
+                             .CellBelow().SetValue(2)
+                             .CellBelow().SetValue(3)
+                             .CellBelow().SetValue(3)
+                             .CellBelow().SetValue(5)
+                             .CellBelow().SetValue(1)
+                             .CellBelow().SetValue(4);
+                ws.Cell("b2").SetValue("a1")
+                             .CellBelow().SetValue("a")
+                             .CellBelow().SetValue("b")
+                             .CellBelow().SetValue("c")
+                             .CellBelow().SetValue("d")
+                             .CellBelow().SetValue("e")
+                             .CellBelow().SetValue("f");
+
+                // Add filters
+                ws.RangeUsed().SetAutoFilter().Column(1).LessThan(4);
+                ws.AutoFilter.Sort(1);
+
+
+
+                wb.SaveAs(@"Y:\Liverpool projects\Windows form app\Ruth\test.xlsx");
+
+            }
+
+            
         }
 
  
