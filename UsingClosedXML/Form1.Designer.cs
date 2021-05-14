6a99567e3415be51db275c6184d322d3c3a8018e
ohin6 <owen.williams-4@postgrad.manchester.ac.uk>
2021-05-14 11:25:36 +0100

    first commit

---
+++ UsingClosedXML/Form1.Designer.cs
@@ -0,0 +1,278 @@
+﻿
+namespace UsingClosedXML
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.Lbl_1 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.box_0101 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.box_0102 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.box_0103 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.box_0104 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.box_0105 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.box_0106 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.box_0107 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.box_0108 = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.Lbl_warning = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 86);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Lbl_1
+            // 
+            this.Lbl_1.AutoSize = true;
+            this.Lbl_1.Location = new System.Drawing.Point(117, 86);
+            this.Lbl_1.Name = "Lbl_1";
+            this.Lbl_1.Size = new System.Drawing.Size(104, 15);
+            this.Lbl_1.TabIndex = 1;
+            this.Lbl_1.Text = "Please Select a File";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(354, 132);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 15);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Ion_0101";
+            // 
+            // box_0101
+            // 
+            this.box_0101.Location = new System.Drawing.Point(427, 129);
+            this.box_0101.Name = "box_0101";
+            this.box_0101.Size = new System.Drawing.Size(100, 23);
+            this.box_0101.TabIndex = 3;
+            this.box_0101.Text = "1";
+            this.box_0101.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(139, 221);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(127, 40);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // box_0102
+            // 
+            this.box_0102.Location = new System.Drawing.Point(427, 158);
+            this.box_0102.Name = "box_0102";
+            this.box_0102.Size = new System.Drawing.Size(100, 23);
+            this.box_0102.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(354, 161);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 15);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Ion_0102";
+            // 
+            // box_0103
+            // 
+            this.box_0103.Location = new System.Drawing.Point(427, 187);
+            this.box_0103.Name = "box_0103";
+            this.box_0103.Size = new System.Drawing.Size(100, 23);
+            this.box_0103.TabIndex = 8;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(354, 190);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 15);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Ion_0103";
+            // 
+            // box_0104
+            // 
+            this.box_0104.Location = new System.Drawing.Point(427, 216);
+            this.box_0104.Name = "box_0104";
+            this.box_0104.Size = new System.Drawing.Size(100, 23);
+            this.box_0104.TabIndex = 10;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(354, 219);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(53, 15);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Ion_0104";
+            // 
+            // box_0105
+            // 
+            this.box_0105.Location = new System.Drawing.Point(427, 245);
+            this.box_0105.Name = "box_0105";
+            this.box_0105.Size = new System.Drawing.Size(100, 23);
+            this.box_0105.TabIndex = 12;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(354, 248);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(53, 15);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Ion_0105";
+            // 
+            // box_0106
+            // 
+            this.box_0106.Location = new System.Drawing.Point(427, 274);
+            this.box_0106.Name = "box_0106";
+            this.box_0106.Size = new System.Drawing.Size(100, 23);
+            this.box_0106.TabIndex = 14;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(354, 277);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(53, 15);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "Ion_0106";
+            // 
+            // box_0107
+            // 
+            this.box_0107.Location = new System.Drawing.Point(427, 303);
+            this.box_0107.Name = "box_0107";
+            this.box_0107.Size = new System.Drawing.Size(100, 23);
+            this.box_0107.TabIndex = 16;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(354, 306);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(53, 15);
+            this.label7.TabIndex = 15;
+            this.label7.Text = "Ion_0107";
+            // 
+            // box_0108
+            // 
+            this.box_0108.Location = new System.Drawing.Point(427, 332);
+            this.box_0108.Name = "box_0108";
+            this.box_0108.Size = new System.Drawing.Size(100, 23);
+            this.box_0108.TabIndex = 18;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(354, 335);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(53, 15);
+            this.label8.TabIndex = 17;
+            this.label8.Text = "Ion_0108";
+            // 
+            // Lbl_warning
+            // 
+            this.Lbl_warning.AutoSize = true;
+            this.Lbl_warning.Location = new System.Drawing.Point(287, 28);
+            this.Lbl_warning.Name = "Lbl_warning";
+            this.Lbl_warning.Size = new System.Drawing.Size(0, 15);
+            this.Lbl_warning.TabIndex = 19;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Lbl_warning);
+            this.Controls.Add(this.box_0108);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.box_0107);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.box_0106);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.box_0105);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.box_0104);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.box_0103);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.box_0102);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.box_0101);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Lbl_1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label Lbl_1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox box_0101;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox box_0102;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox box_0103;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox box_0104;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox box_0105;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox box_0106;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox box_0107;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox box_0108;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label Lbl_warning;
+    }
+}
+
---
+++ UsingClosedXML/Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.IO;
+using System.Data;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.ComponentModel;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ClosedXML.Excel;
+using ClosedXML;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace UsingClosedXML
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //SELECT FILE AND STORE
+            var fileContent = string.Empty;
+            string[] filePath;
+
+            if (Lbl_1.Text == "Please Select a File")
+            {
+                Lbl_warning.ForeColor = Color.Red;
+                Lbl_warning.Text = "Please select a file before continuing";
+                return;
+            }
+
+            Lbl_warning.Text = "";
+
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.InitialDirectory = "c:\\";
+                openFileDialog.Filter = "excel|*.xls";
+                openFileDialog.Multiselect = true;
+                openFileDialog.FilterIndex = 2;
+                openFileDialog.RestoreDirectory = true;
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    //Get the path of specified file, View it
+                    filePath = openFileDialog.FileNames;
+                    string filePathString = string.Join("\n", filePath);
+                    Lbl_1.Text = filePathString;
+                }
+                
+            }
+        }
+
+      
+        private void button2_Click(object sender, EventArgs e)
+        {
+            IXLWorkbook wb = new XLWorkbook();//Create workbook used closedXML
+            IXLWorksheet ws = wb.Worksheets.Add("Sample Sheet");//In workbook create worksheet and give name
+           //To delete worksheet ---> wb.Worksheet("Sample Sheet").Delete();
+
+
+            //Give headers using textbox inputs
+
+            ws.Cell(1, 1).Value = box_0101.Text;
+            ws.Cell(1, 2).Value = box_0102.Text;
+            ws.Cell(1, 3).Value = box_0103.Text;
+            ws.Cell(1, 4).Value = box_0104.Text;
+            ws.Cell(1, 5).Value = box_0105.Text;
+            ws.Cell(1, 6).Value = box_0106.Text;
+            ws.Cell(1, 7).Value = box_0107.Text;
+            ws.Cell(1, 8).Value = box_0108.Text;
+
+            //select range in cells
+            //IXLRange rng = ws.Range("A1:H1");
+            var firstCell = ws.FirstCellUsed();
+            var lastCell = ws.LastCellUsed();
+            var range = ws.Range(firstCell.Address, lastCell.Address);
+
+            range.Style.Border.OutsideBorder = XLBorderStyleValues.Medium;
+
+
+            ws.Row(1).Sort();
+
+            wb.SaveAs(@"Y:\Liverpool projects\Windows form app\Ruth\test.xlsx");
+        }
+
+ 
+    }
+}
+
+
+
+
+           
