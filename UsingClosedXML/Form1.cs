d28a0aef8e05222b6db37f13450424fd0f9e5f79
ohin6 <owen.williams-4@postgrad.manchester.ac.uk>
2021-05-16 12:26:37 +0100

    Add Headers nd Transpose table

--- UsingClosedXML/Form1.cs
+++ UsingClosedXML/Form1.cs
@@ -92,58 +92,94 @@ namespace UsingClosedXML
                 DataTable dataTable = worksheet.Cells.ExportDataTable(0, 0, numRows, numColumn, true);
 
                 //Filter
-                var results = dataTable.Select(@"Ioncode_0101 < 100");
-                
-                var results2 = dataTable.Select(@"Ioncode_0102 < 100");
-                var results3 = dataTable.Select(@"Ioncode_0103 < 100");
-                var results4 = dataTable.Select(@"Ioncode_0104 < 100");
-                var results5 = dataTable.Select(@"Ioncode_0105 < 100");
-                var results6 = dataTable.Select(@"Ioncode_0106 < 100");
-                var results7 = dataTable.Select(@"Ioncode_0107 < 100");
-                var results8 = dataTable.Select(@"Ioncode_0108 < 100");
-
-
-
-                // Bind the DataTable with DataGrid
-                dataGridView1.DataSource = dataTable;
+                var Ioncode_0101 = dataTable.Select(@"Ioncode_0101 < 100");
+                var Ioncode_0102 = dataTable.Select(@"Ioncode_0102 < 100");
+                var Ioncode_0103 = dataTable.Select(@"Ioncode_0103 < 100");
+                var Ioncode_0104 = dataTable.Select(@"Ioncode_0104 < 100");
+                var Ioncode_0105 = dataTable.Select(@"Ioncode_0105 < 100");
+                var Ioncode_0106 = dataTable.Select(@"Ioncode_0106 < 100");
+                var Ioncode_0107 = dataTable.Select(@"Ioncode_0107 < 100");
+                var Ioncode_0108 = dataTable.Select(@"Ioncode_0108 < 100");
 
                 // Close the file stream to free all resources
                 fstream.Close();
 
 
 
-                
-
-
-
-
-                // Copy the table to another worksheet
+                // Copy the table to another worksheet THIS SECTION IS NOT RELEVANT
                 var wsCopy = wb.Worksheets.Add("ws2");
                 wsCopy.Cell(1, 1).Value = rngData;
 
                 var wsCopy2 = wb.Worksheets.Add("ws3");
                 wsCopy2.Cell(1, 1).Value = firstColumn;
 
+                
+                //
+                //Paste datatable queries to Worksheet (ws4) and paste gene target into worksheet (ws5)
+                
+                //1. create worksheet ws4 and paste dataTable query result (line 95 to 102)
+                // first query is Ioncode_0101
                 var wsCopy3 = wb.Worksheets.Add("ws4");
-                wsCopy3.Cell(1, 1).Value = results;
+                wsCopy3.Cell(1, 1).Value = Ioncode_0101; //paste row1 column 1
 
-                //Copying first row in IonCode_0101 and pasting it to another ws
-                var geneResult1 = wsCopy3.RangeUsed().Column(1);
+                //2. copy gene target (column 1) and paste into new worksheet (ws5)
+                var geneIoncode = wsCopy3.RangeUsed().Column(1); 
                 var wsCopy4 = wb.Worksheets.Add("ws5");
-                wsCopy4.Cell(1, 1).Value = geneResult1;
+                wsCopy4.Cell(1,1).Value = geneIoncode;
+
+                //3. Repeat this process for all database queries - should be made into a loop
+                //3a. Ioncode_0102
+                wb.Worksheet("ws4").Clear();     // clear previous query results from ws4           
+                wsCopy3.Cell(1, 1).Value = Ioncode_0102; //paste new query to ws4
+                wsCopy4.Cell(1, 2).Value = geneIoncode; //paste gene target from ws4 to ws5 column 2
+
+                //3b. Ioncode_0103
+                wb.Worksheet("ws4").Clear();
+                wsCopy3.Cell(1, 1).Value = Ioncode_0103;
+                wsCopy4.Cell(1, 3).Value = geneIoncode;
+
+                //3c. Ioncode_0104
+                wb.Worksheet("ws4").Clear();
+                wsCopy3.Cell(1, 1).Value = Ioncode_0104;
+                wsCopy4.Cell(1, 4).Value = geneIoncode;
+
+                //3d. Ioncode_0105 
+                wb.Worksheet("ws4").Clear();
+                wsCopy3.Cell(1, 1).Value = Ioncode_0105;
+                wsCopy4.Cell(1, 5).Value = geneIoncode;
+
+                //3e. Ioncode_0106 
+                wb.Worksheet("ws4").Clear();
+                wsCopy3.Cell(1, 1).Value = Ioncode_0106;
+                wsCopy4.Cell(1, 6).Value = geneIoncode;
+
+                //3f. Ioncode_0107 
+                wb.Worksheet("ws4").Clear();
+                wsCopy3.Cell(1, 1).Value = Ioncode_0107;
+                wsCopy4.Cell(1, 7).Value = geneIoncode;
+
+                //3g. Ioncode_0108
+                wb.Worksheet("ws4").Clear();
+                wsCopy3.Cell(1, 1).Value = Ioncode_0108;
+                wsCopy4.Cell(1, 8).Value = geneIoncode;
 
 
 
+                //Insert sample names as Headers from text file
+                var Worksheet5 = wb.Worksheet(5); //making ws5 a variable called Worksheet5 as this allows to modify
+                Worksheet5.Cell(1, 1).Value = box_0101.Text;
+                Worksheet5.Cell(1, 2).Value = box_0102.Text;
+                Worksheet5.Cell(1, 3).Value = box_0103.Text;
+                Worksheet5.Cell(1, 4).Value = box_0104.Text;
+                Worksheet5.Cell(1, 5).Value = box_0105.Text;
+                Worksheet5.Cell(1, 6).Value = box_0106.Text;
+                Worksheet5.Cell(1, 7).Value = box_0107.Text;
+                Worksheet5.Cell(1, 8).Value = box_0108.Text;
 
-
-                //ws.Cell(1, 1).Value = box_0101.Text;
-                //ws.Cell(1, 2).Value = box_0102.Text;
-                //ws.Cell(1, 3).Value = box_0103.Text;
-                //ws.Cell(1, 4).Value = box_0104.Text;
-                //ws.Cell(1, 5).Value = box_0105.Text;
-                //ws.Cell(1, 6).Value = box_0106.Text;
-                //ws.Cell(1, 7).Value = box_0107.Text;
-                //ws.Cell(1, 8).Value = box_0108.Text;
+                //Transpose data
+                var Worksheet5Range = Worksheet5.RangeUsed();
+                Worksheet5Range.Transpose(XLTransposeOptions.MoveCells);
+                Worksheet5.Columns().AdjustToContents();
 
 
                 wb.SaveAs(@"Y:\Liverpool projects\Windows form app\Ruth\test.xlsx");
